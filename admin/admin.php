90f389ace4c236f70f4089ae6161271fbb43e920
Thibaut MINARD <minard.thibaut@free.fr>
2020-02-04 22:40:01 +0100

    Admin + CSS

--- admin/admin.php
+++ admin/admin.php
@@ -3,16 +3,16 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="style.css" rel="stylesheet" /> 
-        <script src="https://cdn.tiny.cloud/1/kkhvihv2kq63k749dzof0jfnzs1w0ws3wkdzmo2al6neam7j/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>e
+        <link href="../css/style.css" rel="stylesheet" /> 
+        <script src="https://cdn.tiny.cloud/1/kkhvihv2kq63k749dzof0jfnzs1w0ws3wkdzmo2al6neam7j/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     </head>
         
     <body>
         ICI votre futur page d'admnistration
 
-        <h1>TinyMCE Quick Start Guide</h1>
-        <form method="post">
-        <textarea id="mytextarea">Hello, World!</textarea>
+        <h1>Tapez votre nouvelle article dans la fenêtre ci-dessous :</h1>
+        <form method="post" id="tinymce">
+        <textarea id="textarea">Hello, World!</textarea>
         </form>
     </body>
     <script src="tinymce.js"></script>
