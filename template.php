896c9c9615959dd48b45e85f37fa81784f92d7b6
Thibaut MINARD <minard.thibaut@free.fr>
2020-02-05 20:43:09 +0100

    Template Banner

--- template.php
+++ template.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title><?= $title ?></title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
+
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+        <div id="banner">
+            <img src="img/emerald-lake-436200.jpg" id="lake">
+            <span id="banner_span">Un billet pour l'Alaska</span>
+        </div>
+    </header>
+    <div id="container">
         <?= $content ?>
-    </body>
+    </div>
+</body>
+
 </html>
