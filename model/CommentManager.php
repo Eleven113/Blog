52c0f79947d900d4f396a460ca12e2a850073bf6
Thibaut MINARD <minard.thibaut@free.fr>
2020-02-06 16:12:53 +0100

    Progress postView

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -6,7 +6,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS date_format_fr FROM comments WHERE id_post = ? ORDER BY date_creation DESC');
         $comments->execute(array($postId));
 
         return $comments;
--- view/postView.php
+++ view/postView.php
@@ -1,31 +1,37 @@
 <?php $title = 'Billet Simple pour l\'Alaska - Jean Forteroche'; ?>
 
 <?php ob_start(); ?>
-    <p><a href="index.php">Retour à la liste des billets</a></p>
+    <p><a href="index.php">Retour à la liste des articles</a></p>
 
-    <div class="news">
-        <h3>
+    <div class="post">
+        <h1>
             <?= htmlspecialchars($post['title']) ?>
-        </h3>
+        </h1>
             
         <p>
-            <?= nl2br(htmlspecialchars($post['content'])) ?>
+            <?= nl2br(htmlspecialchars($post['post'])) ?>
         </p>
     </div>
+    <div id="comments">
+    <div id="comments_post">
+        <h2>Commentaires</h2>
 
-    <h2>Commentaires</h2>
-
-    <?php
-        while ($comment = $comments->fetch())
-        {
-    ?>
-        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-    <?php
-        }
-        $comments->closeCursor();
-    ?>
-    <?php $content = ob_get_clean(); ?>
+        <?php
+            while ($comment = $comments->fetch())
+            {
+        ?>
+            <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['date_format_fr'] ?></p>
+            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+        <?php
+            }
+            $comments->closeCursor();
+        ?>
+        <?php $content = ob_get_clean(); ?>
+    </div>    
+    <div id="comments_form">
+        <form>
+        </form>
+    </div>
         
     <?php require('template.php'); ?>
 
